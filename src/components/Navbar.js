5c56927033e24b75da6d45ca68c3d9a94bd83be9
Hairun Huang <hairunhuang@gmail.com>
2024-09-07 21:16:59 -0700

    add testimonial

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,9 +8,9 @@ import { cn } from '@/lib/utils'
 async function Navbar() {
 
     return (
-        <nav className="sticky h-16 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-sm transition-all">
+        <nav className="sticky h-20 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-sm transition-all">
             <MaxWidthWrapper>
-                <div className='flex h-16 items-center justify-between'>
+                <div className='flex h-20 items-center justify-between'>
                     <div className='flex items-center justify-center gap-14'>
                         <Link href='/' className='flex z-40 font-bold text-lg'>
                             <span>Your App</span>
--- src/components/landingPageComponents/Testimonials.js
+++ src/components/landingPageComponents/Testimonials.js
@@ -19,19 +19,115 @@ const tweets = [
     },
     {
         id: '1809503790862578002',
+    },
+    {
+        id: '1828089354713215171'
     }
 ]
 function Testimonials() {
     return (
         <MaxWidthWrapper>
-            <div className='text-center space-y-5 my-14'>
+            <div className='text-center space-y-5 my-14' id='testimonials'>
                 <h1 className='font-bold text-4xl'>6,893 happy customers</h1>
                 <h2 className='font-semibold text-xl'>Hear from our satisfied customers about their experience with our products and services.</h2>
             </div>
-            
-            <div className='mx-auto md:columns-2 lg:columns-3 xl:columns-4 space-y-4 md:space-y-6 md:gap-6'>
 
-            </div>
+            <ul className='mx-auto md:columns-2 lg:columns-3 space-y-4 md:space-y-6 md:gap-6'>
+                <li className='break-inside-avoid'>
+                    <figure className="relative h-full w-full max-w-[500px] p-6 rounded-xl border border-gray-200">
+                        <blockquote className='border-b pb-4 font-semibold text-lg'>
+                            This product changed my life Lorem ipsum dolor sit.
+                        </blockquote>
+                        <figcaption>
+                            <div className='flex items-center gap-4 mt-4'>
+                                <img src="/users/user-1.png" alt="user" className="inline-block shrink-0 pointer-events-none h-12 w-12 rounded-full ring-2 ring-gray-300" />
+                                <div className="flex flex-col">
+                                    <p className='font-semibold'>John D</p>
+                                    <p className='text-sm'>@JohnD</p>
+                                </div>
+                            </div>
+                        </figcaption>
+                    </figure>
+                </li>
+
+                <li className='break-inside-avoid'>
+                    <Tweet id={tweets[0].id} />
+                </li>
+
+                <li className='break-inside-avoid'>
+                    <Tweet id={tweets[2].id} />
+                </li>
+
+                <li className='break-inside-avoid'>
+                    <figure className="relative h-full w-full max-w-[500px] p-6 rounded-xl border border-gray-200">
+                        <blockquote className='border-b pb-4 font-semibold text-lg'>
+                            This product changed my life Lorem ipsum dolor sit.
+                        </blockquote>
+                        <figcaption>
+                            <div className='flex items-center gap-4 mt-4'>
+                                <img src="/users/user-3.png" alt="user" className="inline-block shrink-0 pointer-events-none h-12 w-12 rounded-full ring-2 ring-gray-300" />
+                                <div className="flex flex-col">
+                                    <p className='font-semibold'>Tracy</p>
+                                    <p className='text-sm'>@tracy</p>
+                                </div>
+                            </div>
+                        </figcaption>
+                    </figure>
+                </li>
+
+                <li className='break-inside-avoid'>
+                    <Tweet id={tweets[1].id} />
+                </li>
+
+                <li className='break-inside-avoid'>
+                    <Tweet id={tweets[3].id} />
+                </li>
+
+                <li className='break-inside-avoid'>
+                    <Tweet id={tweets[4].id} />
+                </li>
+
+                <li className='break-inside-avoid'>
+                    <figure className="relative h-full w-full max-w-[500px] p-6 rounded-xl border border-gray-200">
+                        <blockquote className='border-b pb-4 font-semibold text-lg'>
+                            This product changed my life Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, voluptatibus
+                        </blockquote>
+                        <figcaption>
+                            <div className='flex items-center gap-4 mt-4'>
+                                <img src="/users/user-2.png" alt="user" className="inline-block shrink-0 pointer-events-none h-12 w-12 rounded-full ring-2 ring-gray-300" />
+                                <div className="flex flex-col">
+                                    <p className='font-semibold'>Jess D</p>
+                                    <p className='text-sm'>@JessD</p>
+                                </div>
+                            </div>
+                        </figcaption>
+                    </figure>
+                </li>
+
+                <li className='break-inside-avoid'>
+                    <Tweet id={tweets[5].id} />
+                </li>
+                <li className='break-inside-avoid'>
+                    <Tweet id={tweets[6].id} />
+                </li>
+
+                <li className='break-inside-avoid cursor-pointer'>
+                    <figure className="relative h-full w-full max-w-[500px] p-6 rounded-xl border border-gray-200 hover:bg-gray-100 transition-all duration-300 ease-in-out transform hover:rotate-2 hover:scale-105 hover:shadow-lg">
+                        <blockquote className='border-b pb-4 font-semibold text-lg'>
+                            This could be you. Try it for free today! Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
+                        </blockquote>
+                        <figcaption>
+                            <div className='flex items-center gap-4 mt-4'>
+                                <img src="/users/user-5.jpg" alt="user" className="inline-block shrink-0 pointer-events-none object-cover h-12 w-12 rounded-full ring-2 ring-gray-300" />
+                                <div className="flex flex-col">
+                                    <p className='font-semibold'>Your name</p>
+                                    <p className='text-sm'>@You</p>
+                                </div>
+                            </div>
+                        </figcaption>
+                    </figure>
+                </li>
+            </ul>
         </MaxWidthWrapper>
     );
 }
