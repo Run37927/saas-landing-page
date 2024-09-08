3d3f24b190add8e1a16eba1956a46a2a81f114f1
Hairun Huang <hairunhuang@gmail.com>
2024-09-07 18:17:43 -0700

    add umami

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,9 @@ export const metadata = constructMetadata();
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className='light !scroll-smooth'>
+      <head>
+        <script defer src="https://cloud.umami.is/script.js" data-website-id="22623642-4859-4b11-bc9c-5e1be448cb2c"></script>
+      </head>
       <body className={cn('min-h-screen font-sans antialiased', mulish.className)}>
         <Toaster />
         <Navbar />
